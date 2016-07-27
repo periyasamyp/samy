fab1aea6f7d82689cfc05ab80f1d1f8f2c61ac5c
periyasamyp <periyasamy753@gmail.com>
2016-07-27 14:43:39 +0530

    Create samy.java

---
+++ samy.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;'
+public class fact
+{
+public static void main(String args[])
+{
+ Scanner sc=new Scanner(System.in);
+ int n,i=1,sum=1;
+ for(i=1;i<=n;i++)
+ {
+ sum=sum *i;
+ }
+ System.out.println(sum);
+ }
+ }
